468d1af530650e09ee1660ee61b3b499622823a6
ShaWn <smorrison1986@gmail.com>
2020-03-20 12:57:28 -0500

    Initializing Avalonia app.

---
+++ StackOfflineFlow/StackOfflineFlow/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace StackOfflineFlow.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ StackOfflineFlow/StackOfflineFlow/Views/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace StackOfflineFlow.Views
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
